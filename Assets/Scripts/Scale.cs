c62c77049eb83ac999569cd92a0b7ffda69af6d4
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-02-07 05:44:48 -0600

    Torus Mutation!
    Good example of targeted noise injection.

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -18,6 +18,8 @@ using LibNoise.Generator;
 
 public class Scale : MonoBehaviour {
 
+    [Range(0,4)]
+    public float strengthAdjustment;
     public MeshRenderer meshRenderer;
     AutoResetEvent resetEvent;
     Transform t;
@@ -26,21 +28,45 @@ public class Scale : MonoBehaviour {
     Vector3 latestScale;
     bool stop;
     Noise2D noiseMap;
+    Noise2D noiseMapTransfer;
+    ModuleBase modBaseTransfer;
     ModuleBase baseModule;
     Texture2D texture;
     bool updatedNoiseMapAvailable;
     bool threadRunning;
-    
-	void Start ()
+    Texture2D[] textureCyles;
+    MeshFilter meshFilter;
+    FractalTorus ftorus;
+    int depth;
+    public int fractalItterations;
+
+    private void Awake()
     {
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+        ftorus = gameObject.GetComponent<FractalTorus>();
+    }
+
+    void Start ()
+    {
+        
         t = transform;
         latestScale = t.localScale;
-        texture = new Texture2D(400, 200);
+        texture = new Texture2D(200, 100);
         thread = new Thread(Run);
         thread.Start();
-        resetEvent = new AutoResetEvent(false);   
+        resetEvent = new AutoResetEvent(false);
+        StartCoroutine(SpawnFractals());
+
     }
     
+    private IEnumerator SpawnFractals()
+    {
+        if (depth < fractalItterations)
+        {
+            new GameObject("Fractal Torus").AddComponent<Scale>();
+        }
+        yield return null;
+    }
 
     void Run()
     {
@@ -49,9 +75,15 @@ public class Scale : MonoBehaviour {
             while (threadRunning)
             {
                 DateTime time = DateTime.Now;
-                baseModule = new Perlin(2, 2.3, .5, 3, time.GetHashCode(), QualityMode.High);
-                noiseMap = new Noise2D(4000, 2000, baseModule);
+                System.Random random = new System.Random(time.GetHashCode());
+                baseModule = new Perlin(2 + random.Next(250,1000)/1000.0f, .5*strengthAdjustment, .5, 5, 3, QualityMode.Low);
+                RidgedMultifractal ridged = new RidgedMultifractal(3 * random.Next(30, 50), .5, random.Next(1, 3), 3, QualityMode.Low);
+                baseModule = new Add(baseModule, ridged);
+
+                noiseMap = new Noise2D(200, 100, baseModule);
                 noiseMap.GenerateSpherical(-90, 90, -180, 180);
+                noiseMapTransfer = noiseMap;
+                modBaseTransfer = baseModule;
                 updatedNoiseMapAvailable = true;
                 threadRunning = false;
             }
@@ -61,16 +93,21 @@ public class Scale : MonoBehaviour {
 	void Update ()
     {
         transform.Rotate(Vector3.down * Time.deltaTime);
+        transform.Rotate(Vector3.left * Time.deltaTime * 1.8f);
+        transform.Rotate(Vector3.back * Time.deltaTime * strengthAdjustment*100);
         if (updatedNoiseMapAvailable)
         {
-            meshRenderer.sharedMaterial.mainTexture = noiseMap.GetTexture();
+            meshRenderer.sharedMaterial.mainTexture = noiseMapTransfer.GetTexture();
+            ftorus.GenerateFractalTorus(modBaseTransfer, strengthAdjustment);
             updatedNoiseMapAvailable = false;
         }
         else if (!threadRunning)
         {
             threadRunning = true;
         }
+        
 	}
+   
 
     public void OnDestroy()
     {
@@ -86,3 +123,4 @@ public class Scale : MonoBehaviour {
         thread.Abort();
     }
 }
+
